1ad0ed931d40001c7dc5b4a76fbebd88499b2c2d
Freid <polifrycommon@gmail.com>
2025-12-18 13:33:54 +0200

    removed certificate required

--- CommentAPI/Extencions/LoadModules/KestrelConfig.cs
+++ CommentAPI/Extencions/LoadModules/KestrelConfig.cs
@@ -2,8 +2,8 @@
 {
     public class KestrelConfig(IConfiguration configuration)
     {
-        internal string certBase64 = configuration["Kestrel:CertBase64"] ?? throw new ArgumentNullException("CertBase64");
-        internal string certPass = configuration["Kestrel:CertPassword"] ?? throw new ArgumentNullException("CertPassword");
+        internal string? certBase64 = configuration["Kestrel:CertBase64"];
+        internal string? certPass = configuration["Kestrel:CertPassword"];
         internal string httpDomen = configuration["Kestrel:HttpDomen"] ?? throw new ArgumentNullException("HttpDomen");
         internal string httpDomenSecure = configuration["Kestrel:httpDomenSecure"] ?? throw new ArgumentNullException("httpDomenSecure");
         internal int port = int.Parse(configuration["PORT"] ?? throw new ArgumentNullException("PORT"));
