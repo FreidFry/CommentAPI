d12da6f0bc03deb2758fa107b197bf5b954c5e1d
Freid <polifrycommon@gmail.com>
2025-12-18 12:38:28 +0200

    fix error where s3 "public url" named "service url"

--- CommentAPI/Extencions/LoadModules/ApiOptions.cs
+++ CommentAPI/Extencions/LoadModules/ApiOptions.cs
@@ -23,12 +23,12 @@ namespace CommentAPI.Extencions.LoadModules
             ImageAccessKeyId =      config["S3:image:AccessKeyId"] ?? throw new ArgumentNullException("S3__Image__AccessKeyId");
             ImageSecretAccessKey =  config["S3:image:SecretAccessKey"] ?? throw new ArgumentNullException("S3__Image__SecretAccessKey");
             ImageStorageUrl =       config["S3:image:StorageUrl"] ?? throw new ArgumentNullException("S3__Image__StorageUrl");
-            ImagePublicUrl =        config["S3:image:ServiceUrl"] ?? throw new ArgumentNullException("S3__Image__PublicUrl");
+            ImagePublicUrl =        config["S3:image:PublicUrl"] ?? throw new ArgumentNullException("S3__Image__PublicUrl");
             ImageBucketName =       config["S3:image:BucketName"] ?? throw new ArgumentNullException("S3__Image__BucketName");
             txtAccessKeyId =        config["S3:txt:AccessKeyId"] ?? throw new ArgumentNullException("S3__txt__AccessKeyId");
             txtSecretAccessKey =    config["S3:txt:SecretAccessKey"] ?? throw new ArgumentNullException("S3__txt__SecretAccessKey");
             txtStorageUrl =         config["S3:txt:StorageUrl"] ?? throw new ArgumentNullException("S3__txt__StorageUrl");
-            txtPublicUrl =          config["S3:txt:ServiceUrl"] ?? throw new ArgumentNullException("S3__txt__PublicUrl");
+            txtPublicUrl =          config["S3:txt:PublicUrl"] ?? throw new ArgumentNullException("S3__txt__PublicUrl");
             txtBucketName =         config["S3:txt:BucketName"] ?? throw new ArgumentNullException("S3__txt__BucketName");
         }
     }
