b57134bd5bdc76946873df9dd55bbe949ac8fa08
Freid <polifrycommon@gmail.com>
2025-12-20 17:16:29 +0200

    change from class to record

--- Comment.Infrastructure/Services/Thread/DTOs/Response/DetailedThreadResponse.cs
+++ Comment.Infrastructure/Services/Thread/DTOs/Response/DetailedThreadResponse.cs
@@ -1,13 +1,13 @@
 
 namespace Comment.Infrastructure.Services.Thread.DTOs.Response
 {
-    public class DetailedThreadResponse
+    public record DetailedThreadResponse
     {
         public Guid Id { get; set; }
-        public string Title { get; set; } = string.Empty;
-        public string Context { get; set; } = string.Empty;
+        public string Title { get; set; }
+        public string Context { get; set; }
         public Guid OwnerId { get; set; }
-        public string OwnerUserName { get; set; } = string.Empty;
+        public string OwnerUserName { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? LastUpdatedAt { get; set; }
         public int CommentCount { get; set; }
