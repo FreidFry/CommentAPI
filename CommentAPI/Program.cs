27fcaf67a1970995a8d83e1f7068a3f7c966b300
Freid <polifrycommon@gmail.com>
2025-12-25 02:35:55 +0200

    fix database connect string

--- CommentAPI/Program.cs
+++ CommentAPI/Program.cs
@@ -60,7 +60,7 @@ builder.Services.AddDipedencyInjections();
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(apiConfig.DbConnection));
 
 
 builder.Services.AddCors(options =>
